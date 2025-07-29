3a1eef75efc61d7c5445af1531feb0cbe4f8ee0e
Jyotisk <sjyotiska@gmail.com>
2025-07-29 12:03:18 +0530

    ttt

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
-const tourRouter=require('./routes/tourRouts')
-const userRouter=require('./routes/userRouts')
+const tourRouter = require('./routes/tourRouts')
+const userRouter = require('./routes/userRouts');
+const morgan = require("morgan");
+
 app.use(express.json());
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
 app.use(express.static(`${__dirname}/public`))
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tours", tourRouter);
 
-module.exports=app;
+module.exports = app;
