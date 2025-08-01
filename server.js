f0a470da7029d5b1f0bd44d2ab18a283d2e8aa3e
jyotisk <sjyotiska@gmail.com>
2025-08-01 16:55:54 +0530

    natours-test

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 dotenv.config({path:'./config.env'})
 const app =require('./app');
-const DB=process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD)
+//for server
+// const DB=process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD)
+// for local
+const DB=process.env.MONGO_URI
 
 // mongodb.connect(DB,{
 //   useNewUrlParser:true,
@@ -28,11 +31,15 @@ const client = new MongoClient(DB, {
 });
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("natours").command({ ping: 1 });
+    await client.db("natours-test").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    // View db data in console
+    const tours = await client.db("natours-test").collection("tours").find({}).toArray();
+    console.log("Tours data:", tours);
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
