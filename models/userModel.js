0b169e04fc5b1daa697dbf41919878253eaf5306
jyotisk <sjyotiska@gmail.com>
2025-12-23 17:40:26 +0530

    authentication

---
+++ models/userModel.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt= require('bcryptjs')
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "The name field is required"],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "The email field is required"],
+    unique: true,
+    validate: [validator.isEmail, "Please provid a valid email"],
+  },
+  photo: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password"],
+    minlength: 8,
+  },
+  passwordconfirm: {
+    type: String,
+    required: [true, "Please comfirm password"],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "password are not the same"
+    },
+  },
+});
+
+userSchema.pre('save',async function(next){
+    // only run if password is modified
+    if(!this.isModified('password')) return next();
+    this.password=await bcrypt.hash(this.password,12);
+
+    //delete password confirm field
+    this.passwordconfirm=undefined
+    next();
+})
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
--- routes/userRouts.js
+++ routes/userRouts.js
@@ -1,10 +1,11 @@
 const express=require('express')
 const routController=require(`${__dirname}/../controller/userController`)
-
+const authController=require(`${__dirname}/../controller/authController`)
 
 const userRouter = express.Router();
 
 
+userRouter.post('/signup',authController.signup)
 
 userRouter.route("/").get(routController.allUsers).post(routController.createUser);
 userRouter.route("/:id").get(routController.getUsers).patch(routController.updateUser).delete(routController.deleteUser);
